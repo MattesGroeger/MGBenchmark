892b706e332494a30a077f09aa9b52d9ea9e28d2
MattesGroeger <info@mattes-groeger.de>
2013-03-22 17:12:43 +0100

    Initial Commit

---
+++ MGBenchmark/MGBenchmark.h
@@ -0,0 +1,13 @@
+//
+//  MGBenchmark.h
+//  MGBenchmark
+//
+//  Created by Mattes Groeger on 22/03/2013.
+//  Copyright (c) 2013 Mattes Groeger. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MGBenchmark : NSObject
+
+@end
---
+++ MGBenchmark/MGBenchmark.m
@@ -0,0 +1,13 @@
+//
+//  MGBenchmark.m
+//  MGBenchmark
+//
+//  Created by Mattes Groeger on 22/03/2013.
+//  Copyright (c) 2013 Mattes Groeger. All rights reserved.
+//
+
+#import "MGBenchmark.h"
+
+@implementation MGBenchmark
+
+@end
